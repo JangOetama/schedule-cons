0a544976371b66f03976d5ccc8332071fc32b767
JangOetama <46255322+JangOetama@users.noreply.github.com>
2024-09-27 08:53:30 +0700

    update: jam

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -34,26 +34,44 @@
           <select class="selectcustom" @change="handleMasukChange(entry.day, index, $event)" :hidden="entry.schedule === 'OFF' || entry.store === ''">
             <option value="" hidden>Select Masuk</option>
             <option value="6:00">6:00</option>
+            <option value="6:30">6:30</option>
             <option value="7:00">7:00</option>
+            <option value="7:30">7:30</option>
             <option value="8:00">8:00</option>
+            <option value="8:30">8:30</option>
             <option value="9:00">9:00</option>
+            <option value="9:30">9:30</option>
             <option value="10:00">10:00</option>
+            <option value="10:30">10:30</option>
             <option value="11:00">11:00</option>
+            <option value="11:30">11:30</option>
             <option value="12:00">12:00</option>
+            <option value="12:30">12:30</option>
             <option value="13:00">13:00</option>
+            <option value="13:30">13:30</option>
             <option value="14:00">14:00</option>
+            <option value="14:30">14:30</option>
           </select>
           <select class="selectcustom" @change="handlePulangChange(entry.day, index, $event)" :hidden="entry.schedule === 'OFF' || entry.store === ''">
             <option value="" hidden>Select Pulang</option>
             <option value="14:00">14:00</option>
+            <option value="14:30">14:30</option>
             <option value="15:00">15:00</option>
+            <option value="15:30">15:30</option>
             <option value="16:00">16:00</option>
+            <option value="16:30">16:30</option>
             <option value="17:00">17:00</option>
+            <option value="17:30">17:30</option>
             <option value="18:00">18:00</option>
+            <option value="18:30">18:30</option>
             <option value="19:00">19:00</option>
+            <option value="19:30">19:30</option>
             <option value="20:00">20:00</option>
+            <option value="20:30">20:30</option>
             <option value="21:00">21:00</option>
+            <option value="21:30">21:30</option>
             <option value="22:00">22:00</option>
+            <option value="22:30">22:30</option>
           </select>
           <button class="removeButton" @click="removeEntry(day, index)">X</button>
         </div>
@@ -91,8 +109,12 @@
       },
     },
     mounted() {
-      this.fetchSa();
-      this.calculateDaysInNextMonth();
+      if (this.username === "") {
+    this.$router.push('/');
+  } else {
+    this.fetchSa();
+    this.calculateDaysInNextMonth();
+  }
     },
 
     methods: {
